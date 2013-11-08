17d6af381845023ec58d50baaddb9a404421a5bd
gilad.foyer@gmail.com <gilad.foyer@gmail.com@devtable.com>
2013-11-08 23:39:59 +0000

    Initial checkin of the project contents

---
+++ giladfoyer.py
@@ -0,0 +1,18 @@
+import webapp2
+import os
+from google.appengine.ext.webapp import template
+
+
+class MainHandler(webapp2.RequestHandler):
+  def get(self):
+    template_values = {
+      'name': "World",
+    }
+
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+
+
+app = webapp2.WSGIApplication([
+  ('/.*', MainHandler),
+], debug=True)
---
+++ static/geymed.js
@@ -0,0 +1,20 @@
+// smiling face image:http://delhi4cats.files.wordpress.com/2011/05/smiley-face.jpg?w=468&h=468
+// crying face image http://doblelol.com/uploads/20/funny-smiley-face-icons-image-search-results.jpg
+
+function init() {
+    var first=Math.floor(Math.random()*10) + 1;
+    var second = Math.floor(Math.random()*10) + 1;
+    $("#first").text(first);
+    $("#second").text(second);
+    $("#submit").click(function () {
+        if (parseInt($("#result").val()) == first+second) {
+            $("#pic").attr('src','http://www.worldpeace-uk.org/wp-content/uploads/2013/07/smiley-face.jpg');
+        } else {
+   $("#pic").attr('src','http://vector-magz.com/wp-content/uploads/2013/07/sad-face-clip-art.jpg');
+        }
+    });
+}
+
+$(document).ready(function(){
+    init();
+});
