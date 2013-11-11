2c5d5703f8fddc8b2d5ac8af731696151f745992
gilad.foyer@gmail.com <gilad.foyer@gmail.com@devtable.com>
2013-11-11 22:19:17 +0000

    proper js code

--- static/geymed.js
+++ static/geymed.js
@@ -1,15 +1,24 @@
 /**
  * Angry Birds Math gamoe for kids
- * @author Gilad Foyer AKA Geyme
+ * @author Gilad Foyer AKA Geymed
+ * 
  */
+;(function ($) {
+    "use strict";
+
+    // Global `vars` and `consts`    
 var abPics = ['static/images/pink-ab-sw.png',
               'static/images/angry-bird-red.png',
               'static/images/angry-bird-war.png'
               ];
 var pigPics = ['static/images/pig.gif',
                 'static/images/king-pig.png'
+                
               ] ;
-function init() {
+            
+              $.extend({abMath: (function () {  
+                 var abm = {
+ init:function () {
     var first=Math.floor(Math.random()*6) + 1;
     var second = Math.floor(Math.random()*6) + 1;
     $("#first").text(first);
@@ -22,14 +31,14 @@ function init() {
       
         if (parseInt($("#result").val()) == first+second) {
           $("#submitCont").unbind('click');
-            $("#pic").attr('src',getRandomPic(abPics));
+            $("#pic").attr('src',abm.getRandomPic(abPics));
            // $("#imgHolder").animate({top:'+=50'},2000, function() {
              
         //$("#imgHolder").animate({top:'-=50'},2000, function() {});
   //});
-            setTimeout(init,5000);
+            setTimeout(abm.init,5000);
         } else {
-   $("#pic").attr('src',getRandomPic(pigPics));
+   $("#pic").attr('src',abm.getRandomPic(pigPics));
    $("#result").val("");
    $("#result").focus();
     //$("#imgHolder").animate({left:'+=50'},2000, function() {
@@ -38,16 +47,22 @@ function init() {
   //});
         }
     });
-}
+},
 
-function getRandomPic(pics) {
+getRandomPic:function (pics) {
  return  pics[Math.floor(Math.random()*pics.length)];
 }
+
+};
+  return {
+    init:abm.init  
+  }}())});
+}(jQuery));
 //todo add numbers from 1-20 on screen, make images animated
 
 
 $(document).ready(function(){
-    init();
+   $.abMath.init();
 });
 
 //TODO add help button with graphic representation of the exercise (x red balls + y green balls, 8 red balls three of them eaten by worms))
